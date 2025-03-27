d80f1a31f6e26c2d11b5b9be39d6d349b2f0873d
candid <olacandid@gmail.com>
2025-03-27 11:18:17 +0100

    'update-routes'

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import './App.css';
-import {Routes, Route} from 'react-router-dom'
-import Landing from './Landing';
-import Navbar from './Navbar';
-import About from './components/About';
-import Project from './components/Project';
-import Contact from './components/Contact';
-import NotFound from './components/NotFound';
-import Footer from './components/Footer';
-import Home from './components/Home';
-import { Navigate } from 'react-router-dom';
-import Resume from './components/Resume';
+import "./App.css";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./Landing";
+import Navbar from "./Navbar";
+import About from "./components/About";
+import Project from "./components/Project";
+import Contact from "./components/Contact";
+import NotFound from "./components/NotFound";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import { Navigate } from "react-router-dom";
+import Resume from "./components/Resume";
 
-const App = () =>{
-  return(
+const App = () => {
+  return (
     <>
-    <div className='color'>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/home' element={<Navigate to='/'/>} />
-      <Route path='/about' element={<About /> }/>
-      <Route path='/project' element={<Project /> }/>
-      <Route path='/resume' element={<Resume/>}/>
-      <Route path='/contact' element={<Contact /> }/>
-      <Route path='/*' element={<NotFound />}/>
-      {/* <Route path='*' element={<NotFound/> }/>  */}
-       {/* <Route path='/' element={<Landing /> }/> */}
-      
-    </Routes>
-    <Footer/>
-
-    </div>
-
+      <div className="color">
+        <Navbar />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Navigate to="/" />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/project" element={<Project />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/*" element={<NotFound />} />
+            {/* <Route path='*' element={<NotFound/> }/>  */}
+            {/* <Route path='/' element={<Landing /> }/> */}
+          </Routes>
+        </Router>
+        <Footer />
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default App;
